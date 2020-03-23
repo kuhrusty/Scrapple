ae3676e08501e7876ced765f98e823b494ca6bdf
Rusty Ballinger <rusty.ballinger@gmail.com>
2020-03-23 06:16:11 -0700

    Update versions, switch from android.support.v7 to androidx

--- app/src/main/java/com/kuhrusty/scrapple/MainActivity.java
+++ app/src/main/java/com/kuhrusty/scrapple/MainActivity.java
@@ -3,7 +3,7 @@ package com.kuhrusty.scrapple;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.os.Parcelable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
